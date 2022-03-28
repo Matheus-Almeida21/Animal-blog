75b5fea8076bf1ba4dfd8e63051841c1e5bc49f2
Matheus Almeida cavallari <matheus.cavallari7@gmail.com>
2022-03-28 16:11:00 -0300

    Interação aos elementos

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-tabContent[0].classList.add('ativo')
+function tabNavigation(){
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  tabContent[0].classList.add('ativo')
 
-if(tabContent.length && tabMenu.length) {
-  tabContent[0].classList.add('ativo');
-  
-  function activeTab(index) {
-    tabContent.forEach((content) => {
-      content.classList.remove('ativo');
+  if(tabContent.length && tabMenu.length) {
+    tabContent[0].classList.add('ativo');
+    
+    function activeTab(index) {
+      tabContent.forEach((content) => {
+        content.classList.remove('ativo');
+      });
+      tabContent[index].classList.add('ativo');
+    }
+    
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add('ativo');
   }
-  
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
-    });
-  });
 }
+tabNavigation()
 
+function initAccordion(){
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  if (accordionList.length) {
+    accordionList[0].classList.add('ativo')
+    accordionList[0].nextElementSibling.classList.add('ativo')
+    
+    function ativaAccordion(){
+      this.classList.toggle('ativo')
+      this.nextElementSibling.classList.toggle('ativo')
+    }
+    
+    accordionList.forEach((item)=>{
+      item.addEventListener('click', ativaAccordion)
+    })
+  }
+}
+initAccordion()
