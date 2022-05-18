c3c13f093761830c08e4713c21ede3f290ae7336
Matheus Almeida cavallari <matheus.cavallari7@gmail.com>
2022-05-18 11:08:12 -0300

    adicionando tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from "./modules/anima-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import tabNavigation from "./modules/navigation.js";
 import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js"
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 tabNavigation();
 initModal();
+initToolTip()
