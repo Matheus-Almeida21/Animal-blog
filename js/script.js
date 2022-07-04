ff46e1a48eeb0a84bf73a9bdcd0c0bd26ca9dfe0
Matheus Almeida cavallari <matheus.cavallari7@gmail.com>
2022-07-04 07:17:24 -0300

    fazendo requisição de dados de um json

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ import initModal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js"
 import initDropdownMenu from "./modules/dropdown.js"
 import initMenuMobile from "./modules/menu-mobile.js"
-import initAnimaNumeros from "./modules/anima-numeros.js"
 import initFuncionamento from "./modules/funcionamento.js"
+import initFetchAnimais from "./modules/fetch-animais.js"
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -17,5 +17,5 @@ initModal();
 initToolTip()
 initDropdownMenu()
 initMenuMobile()
-initAnimaNumeros()
 initFuncionamento()
+initFetchAnimais()
